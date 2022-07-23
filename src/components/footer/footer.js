801501d54a8370732f94701d1631cc6b76a0aad2
ChuNgocBinh <chungocbinh.167@gmail.com>
2022-07-23 11:31:00 +0700

    footer

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./footer.sass";
+
+function Footer() {
+  return (
+    <div>
+      <div>day la footer</div>
+    </div>
+  );
+}
+
+export default Footer;
--- src/layout/layout.js
+++ src/layout/layout.js
@@ -1,14 +1,15 @@
-import React, { useEffect } from 'react';
-import Header from 'components/header/header';
-import './layout.sass';
-import { IntlProvider } from 'react-intl';
-import { useSelector, useDispatch } from 'react-redux';
-import appLocale from 'i18n/index';
-import messages from 'i18n/entries/entries';
-import Loader from 'components/loader/loader';
-import { NotificationContainer } from 'react-notifications';
-import { fetchUserInfo } from 'redux/appSLice';
-import 'react-notifications/lib/notifications.css';
+import React, { useEffect } from "react";
+import Header from "components/header/header";
+import "./layout.sass";
+import { IntlProvider } from "react-intl";
+import { useSelector, useDispatch } from "react-redux";
+import appLocale from "i18n/index";
+import messages from "i18n/entries/entries";
+import Loader from "components/loader/loader";
+import { NotificationContainer } from "react-notifications";
+import { fetchUserInfo } from "redux/appSLice";
+import "react-notifications/lib/notifications.css";
+import Footer from "components/footer/footer";
 
 function Layout({ children }) {
   const locale = useSelector((state) => state.app.locale);
@@ -23,15 +24,13 @@ function Layout({ children }) {
   return (
     <div>
       {loading && <Loader />}
-      <IntlProvider
-        messages={messages[locale]}
-        locale={appLocale[locale]}
-      >
+      <IntlProvider messages={messages[locale]} locale={appLocale[locale]}>
         <div className="layout_header">
           <Header />
         </div>
-        <div className="layout_body">
-          {children}
+        <div className="layout_body">{children}</div>
+        <div>
+          <Footer />
         </div>
         <NotificationContainer />
       </IntlProvider>
