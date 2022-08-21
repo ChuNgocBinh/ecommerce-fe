408f3839351c25964a7897321cee8b853ffdcb7b
ChuNgocBinh96 <cnb96.cv@gmail.com>
2022-08-21 13:03:04 +0700

    setup successfully

--- src/App.js
+++ src/App.js
@@ -48,6 +48,7 @@ function App() {
         {/* <Route element={<PrivateRouter />}> */}
         {renderPrivateRoute()}
         {/* </Route> */}
+        {/* </Route> */}
       </Routes>
     </Layout>
   );
